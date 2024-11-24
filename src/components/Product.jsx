74471730a209920f9c3300bcfb255f6c775b0ee4
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-11-24 14:42:11 +0500

    done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, message } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { ShoppingOutlined } from '@ant-design/icons';
 import { productType } from "../store/actions/ActionTypes";
@@ -7,13 +7,25 @@ import useSelection from "antd/es/table/hooks/useSelection";
 
 const { Meta } = Card;
 const Product = ({data}) => {
-
+    const {id,title,description,image,price,category} = data 
     const dispatch = useDispatch()
-    const cartData = useSelector((state)=>state)
+
+    // All Cart product here
+    const cartData = useSelector((state)=>state.cart);
+    const handleCartProduct = ()  => {
+      const condition = cartData.some((item) => item.id == id);
+      if (condition) {
+        message.warning("This product is already in the cart!");
+      }
+      else {
+        dispatch({type:productType.ADD_CART_PRODUCT, payload:data})
+        message.success('Added successfully');
+      }
+    }
+
     console.log(cartData);
     
 
-    const {id,title,description,image,price,category} = data 
 
   return (
     <div>
@@ -26,7 +38,7 @@ const Product = ({data}) => {
     <Meta title={title} description={description.slice(0,100)} />
     <div className="flex justify-between py-5 text-2xl">
     <p className="text-red-600">Price : {price}$</p>
-    <p className="text-green-700 top-0 right-0 absolute p-5 text-3xl"><ShoppingOutlined onClick={()=>dispatch({type:productType.CART_PRODUCT, payload:data})}/></p>
+    <p className="text-green-700 top-0 right-0 absolute p-5 text-3xl"><ShoppingOutlined onClick={handleCartProduct}/></p>
     </div>
   </Card>
     </div>
